2ce16333aeb2b61faa1373905f766ee957d25d9a
1373939387 <1373939387@qq.com>
2019-03-12 19:12:42 +0800

    识别本地二维码

--- app/src/main/java/com/caregrowtht/app/activity/QRCodeActivity.java
+++ app/src/main/java/com/caregrowtht/app/activity/QRCodeActivity.java
@@ -17,6 +17,8 @@ import com.android.library.utils.U;
 import com.caregrowtht.app.Constant;
 import com.caregrowtht.app.R;
 import com.caregrowtht.app.uitil.BitmapUtils;
+import com.caregrowtht.app.uitil.StrUtils;
+import com.caregrowtht.app.user.UserManager;
 import com.uuzuche.lib_zxing.activity.CaptureFragment;
 import com.uuzuche.lib_zxing.activity.CodeUtils;
 
@@ -77,31 +79,7 @@ public class QRCodeActivity extends BaseActivity {
     CodeUtils.AnalyzeCallback analyzeCallback = new CodeUtils.AnalyzeCallback() {
         @Override
         public void onAnalyzeSuccess(Bitmap mBitmap, String result) {
-//        org=000H
-            String type = "0";
-            String retentStr = null;
-            if (result.contains(Constant.isTest ? "http" : "https") && result.contains("s=")) {//微信能扫的
-                String[] strs = result.split("=");
-                retentStr = String.valueOf(BitmapUtils.decode(strs[1]));
-                type = "1";
-            } else if (result.contains("&")) {// org=00CC&lesson=2265
-                String lessonId = result.substring(result.lastIndexOf("=")+1);
-                retentStr = lessonId;
-                type = "2";
-            } else {// 微信不能扫的
-                //解密二维码
-                retentStr = String.valueOf(BitmapUtils.decode(result));
-                type = "1";
-            }
-
-            Intent resultIntent = new Intent();
-            Bundle bundle = new Bundle();
-            bundle.putInt(CodeUtils.RESULT_TYPE, CodeUtils.RESULT_SUCCESS);
-            bundle.putString("invitationCode", retentStr);
-            bundle.putString("type", type);// type: 1: org=000H(机构Id) 2: org=00CC&lesson=2265(课程Id)
-            resultIntent.putExtras(bundle);
-            QRCodeActivity.this.setResult(RESULT_OK, resultIntent);
-            QRCodeActivity.this.finish();
+            getQRCode(result, "1");// 扫描
         }
 
         @Override
@@ -116,6 +94,50 @@ public class QRCodeActivity extends BaseActivity {
             QRCodeActivity.this.finish();
         }
     };
+
+    /**
+     * @param result     识别的参数
+     * @param typeQRCode 识别类型
+     */
+    private void getQRCode(String result, String typeQRCode) {
+        //        org=000H
+        String type = "0";
+        String retentStr = null;
+        if (result.contains(Constant.isTest ? "http" : "https") && result.contains("s=")) {//微信能扫的
+            String[] strs = result.split("=");
+            retentStr = String.valueOf(BitmapUtils.decode(strs[1]));
+            type = "1";
+        } else if (result.contains("&")) {// org=00CC&lesson=2265
+            String lessonId = result.substring(result.lastIndexOf("=") + 1);
+            retentStr = lessonId;
+            type = "2";
+        } else {// 微信不能扫的
+            //解密二维码
+            retentStr = String.valueOf(BitmapUtils.decode(result));
+            type = "1";
+        }
+        if (typeQRCode.equals("1")) {
+            Intent resultIntent = new Intent();
+            Bundle bundle = new Bundle();
+            bundle.putInt(CodeUtils.RESULT_TYPE, CodeUtils.RESULT_SUCCESS);
+            bundle.putString("invitationCode", retentStr);
+            bundle.putString("type", type);// type: 1: org=000H(机构Id) 2: org=00CC&lesson=2265(课程Id)
+            resultIntent.putExtras(bundle);
+            QRCodeActivity.this.setResult(RESULT_OK, resultIntent);
+            QRCodeActivity.this.finish();
+        } else {
+            if (type.equals("1")) {
+                if (!StrUtils.isEmpty(retentStr)) {
+                    UserManager.getInstance().getOrgInfo(retentStr, this, "1");
+                } else {
+                    U.showToast("未识别二维码!");
+                }
+            } else {
+                startActivity(new Intent(this, CourserActivity.class)
+                        .putExtra("courseId", retentStr));
+            }
+        }
+    }
 
 
     @OnClick({R.id.tv_title, R.id.tv_scan, R.id.tv_dist, R.id.btn_create_child})
@@ -157,17 +179,7 @@ public class QRCodeActivity extends BaseActivity {
                             new CodeUtils.AnalyzeCallback() {
                                 @Override
                                 public void onAnalyzeSuccess(Bitmap mBitmap, String result) {
-                                    String retentStr = null;
-                                    if (result.indexOf("http") != -1) {//微信能扫的
-                                        String[] html = result.split("[=]");//org000F&registerType、2&commendId、1
-                                        String[] strs = html[1].split("&");
-                                        retentStr = strs[0];
-                                    }
-                                    if (TextUtils.isEmpty(result)) {
-                                        U.showToast("未发现二维码");
-                                    } else {
-                                        U.showToast("二维码" + retentStr);
-                                    }
+                                    getQRCode(result, "2");// 识别二维码
                                 }
 
                                 @Override
